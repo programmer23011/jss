173a8b315f3967f7191517b9d3501d85e75015c4
alfanona <alfanona2011@gmail.com>
2026-05-28 13:49:20 +0300

    2

--- app/vite.config.js
+++ app/vite.config.js
@@ -1,18 +1,7 @@
-import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: 'dancevue',
-  plugins: [
-    vue(),
-    vueDevTools()
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+  plugins: [vue()],
+  base: 'dancE' // Замените на имя вашего репозитория
 })
